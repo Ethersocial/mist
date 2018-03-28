227647b6d4f2855d04ea56593e713a83f17d2544
magnalucus <34496116+ethersocialcoin@users.noreply.github.com>
2018-03-28 15:15:50 +0900

    Change mainnet hash

--- interface/client/lib/helpers/helperFunctions.js
+++ interface/client/lib/helpers/helperFunctions.js
@@ -235,28 +235,16 @@ Helpers.detectNetwork = function(hash) {
   var network = {};
 
   switch (hash) {
-    case '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3':
+    case '0x310dd3c4ae84dd89f1b46cfdd5e26c8f904dfddddc73f323b468127272e20e9f':
       console.log('Network is mainnet');
       network.type = 'mainnet';
       network.name = 'Main';
       break;
 
     case '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d':
-      console.log('Network is Testnet #3 (Ropsten)');
+      console.log('Network is Testnet');
       network.type = 'testnet';
-      network.name = 'Ropsten';
-      break;
-
-    case '0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177':
-      console.log('Network is Testnet #4 (Rinkeby)');
-      network.type = 'testnet';
-      network.name = 'Rinkeby';
-      break;
-
-    case '0x0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303':
-      console.log('Network is Testnet #2 (Morden)');
-      network.type = 'testnet';
-      network.name = 'Morden';
+      network.name = 'Testnet';
       break;
 
     default:
