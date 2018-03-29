fe9a90b459aa5203c76aac42eafd9d882ced7d22
magnalucus <34496116+ethersocialcoin@users.noreply.github.com>
2018-03-29 11:30:18 +0900

    ethereum to magnalucus

--- modules/updateChecker.js
+++ modules/updateChecker.js
@@ -23,7 +23,7 @@ const check = (exports.check = () => {
       break;
   }
 
-  return got('https://api.github.com/repos/ethereum/mist/releases', {
+  return got('https://api.github.com/repos/magnalucus/mist/releases', {
     timeout: 3000,
     json: true
   })
